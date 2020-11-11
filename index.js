8e6a80b2cc7b0391e98f1f0d5b5d0a6d134f96e0
Nicholas Ochs <nick.ochs@yahoo.com>
2020-11-10 20:43:16 -0500

    using es6 rendering engine

--- index.js
+++ index.js
@@ -6,52 +6,72 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
+const est6Renderer = require('express-es6-template-engine');
+// Defining the template engine
+app.engine('html', est6Renderer);
+// Set up a "views" directory
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
 const server = http.createServer(app);
 
 // Let set up our friends from db.js
 const db = require('./db');
 
+// Set up t he Home Page, available at the "/" route
+app.get('/', (req, res) => {
+    // This render() method is how we tell Express to look into the templates directory for a view named 'home', and process it with our rendering engine
+    res.render('home', {
+        locals: {
+            title: 'Address Book'
+        },
+        partials: {
+            head: '/partials/head'
+        }
+    });
+});
+
 app.get('/friends', (req, res) => {
-
-    let htmlString = ``;
-
-    htmlString += `<ul>`;
-    for (let friend of db) {
-        // Put each friends name property into an <li> tag
-        htmlString += `<li>
-            <a href="${req.path}${friend.handle}">${friend.name}</a>
-        </li>`
-    }
-    htmlString += `</ul>`;
-    res.send(htmlString);
-})
+    res.render('friends-list', {
+        locals: {
+            friends: db,
+            title: 'Friends List'
+        },
+        partials: {
+            head: '/partials/head',
+        }
+    });
+});
 
 app.get('/friends/:handle', (req, res, next) => {
     const handle = req.params.handle;
     const friend = db.find((f) => {
         return f.handle === handle;
-    })
+    });
 
     if (!friend) {
         next();
     }
-    
-    htmlString = ``;
-    htmlString += `<h1>${friend.name}</h1>`
-    htmlString += `<h3>${friend.handle}</h3>`
-    htmlString += `<h3>${friend.skill}</h3>`
 
-    res.send(htmlString);
-})
+    res.render('friend', {
+        locals: {
+            friend: friend,
+            title: `${friend.name}'s Profile Page`
+        },
+        partials: {
+            head: '/partials/head'
+        }
+    });
+});
 
 
 app.get('*', (req, res) => {
     console.log(`matched from ${req.path}`)
     res.status(404).send(`<h1>404 - Not Found</h1>`)
-})
+});
 
 
 
 server.listen(port, hostname, () =>{
     console.log(`Server running on http://${hostname}:${port}`);
-})
+});
